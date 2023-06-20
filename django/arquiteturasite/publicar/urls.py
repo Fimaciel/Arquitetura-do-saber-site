3b2dc3216b091e606c317783c10a18fecd369d10
Filipe Maciel <filipemaciellopes01@gmail.com>
2023-06-19 22:26:56 -0400

    Adicionando as telas no django

--- django/arquiteturasite/publicar/urls.py
+++ django/arquiteturasite/publicar/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path("", views.index, name="index"),
+    path("login", views.login, name="login"),
+    path("formacao", views.formacao, name="formacao"),
+    path("formacaoUser", views.formacaoUser, name="formacaoUser"),
+    path("acessoria", views.acessoria, name="acessoria"),
 ]
--- django/arquiteturasite/publicar/views.py
+++ django/arquiteturasite/publicar/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request,'index.html')
+    return render(request, "index.html")
+
+
+def login(request):
+    return render(request, "login.html")
+
+
+def formacao(request):
+    return render(request, "formacaoCliente.html")
+
+
+def formacaoUser(request):
+    return render(request, "acessoriaUsuario.html")
+
+
+def acessoria(request):
+    return render(request, "acessoriaCliente.html")
